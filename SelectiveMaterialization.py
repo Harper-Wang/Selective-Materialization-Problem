b028d3449288be32393500f34a8a384939620f8f
Chuhan，Wang <30429795+Harper-Wang@users.noreply.github.com>
2019-02-17 18:55:15 +0800

    Update SelectiveMaterialization.py

--- SelectiveMaterialization.py
+++ SelectiveMaterialization.py
@@ -1,7 +1,8 @@
 #%%
 import numpy as np 
 import pandas as pd 
-# data=dict(psc=[6],pc=[6],ps=[0.8],sc=[6],p=[0.2],s=[0.01],c=[0.1])
+
+# testdata
 data=dict(abcde=[12], abcd=[9], abce=[10], abde=[8], acde=[7], bcde=[4], abc=[2.5], abd=[3], abe=[5], acd=[2.8], ace=[3], ade=[2.2], bcd=[2], bce=[1.9], bde=[1.7], 
 cde=[3.7], ab=[1.9], ac=[2.1], ad=[0.5], ae=[2.1], bc=[0.7], bd=[0.3], be=[1.4], 
 cd=[0.4], ce=[1.7], de=[0.6], a=[0.1], b=[0.3], c=[0.1], d=[0.2], e=[0.2])
@@ -45,7 +46,7 @@ def updatecost(dict1,x):
 
         
 #%%
-
+# main function
 def selectiveM(dict1,k,topview):
     view_selected = []
     for i in range(k):
@@ -60,7 +61,8 @@ def selectiveM(dict1,k,topview):
         print(dict1)
     return view_selected
 
-print(selectiveM(data,3,topview))
+# get result
+print(selectiveM(data,3,topview)) 
             
 
 
